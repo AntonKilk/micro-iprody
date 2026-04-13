4e78452ba86db0520a6941dacfe6e4b24e663d58
Anton Kilk <ant.kilk@gmail.com>
2026-04-13 16:15:45 +0300

    initial commit. Add order service

---
+++ order-service/src/test/java/com/stellar/crm/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stellar.crm.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
